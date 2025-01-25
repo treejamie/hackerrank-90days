bbd20996a0245bc97fc6c48ab2e9ca81a1368d4c
Jamie <1866+treejamie@users.noreply.github.com>
2025-01-25 09:52:58 +0000

    Divisible numbers 'On' solution (#3)
    * Added in some data sets and an 'On' solution
    
    * remove the reduce

--- w1/6_divisible_pairs.py
+++ w1/6_divisible_pairs.py
@@ -7,7 +7,50 @@ import sys
 
 
 
+def divisibleSumPairs2(n, k, ar):
+    """
+    This is a O(n) version using a map as a lookup table
+
+    There was a few videos about this, but I struggled to get the underlying logic
+    so here I battled it out on paper until I got the very basic algo working.
+
+    What I can say is that I've never once, since 1998 had to use something like this
+    in any application I've built. That's either saying more about me than it is the test :) 
+
+    """
+    # set the values up
+    db = {}
+    results = []
+
+    # iterate over ar
+    for value in ar:
+        # get the mod first
+        mod = value % k
+
+        # now get the complement
+        # but if mod is zero, use zero instead
+        comp = 0 if mod == 0 else k - mod
+
+        # if the complement is in the db as a value, append the value of it to results
+        hit = db.get(comp)
+        if hit is not None:
+            results.append(hit)
+
+        # add the mod into the db, default to one, but
+        try:
+            db[mod]
+            db[mod] += 1
+        except KeyError:
+            db[mod] = 1
+
+    return sum(results)
+
+
+
 def divisibleSumPairs1(n, k, ar):
+    """
+    I don't think the O n squared is going to win me any friends.
+    """
     # the results
     results = []
 
@@ -20,9 +63,8 @@ def divisibleSumPairs1(n, k, ar):
             if (ar[i] + ar[j]) % k == 0:
                 results.append((i, k))
 
-    return results
+    return len(results)
         
-
 
 
 def parse_input(fpath):
@@ -47,15 +89,17 @@ if __name__ == '__main__':
         "w1/6_divisible_pairs_data_set_test_1.txt",
         "w1/6_divisible_pairs_data_set_test_2.txt",
         "w1/6_divisible_pairs_data_set_test_6.txt",
-        "w1/6_divisible_pairs_data_set_test_19.txt"
+        "w1/6_divisible_pairs_data_set_test_19.txt",
+        "w1/6_divisible_pairs_data_set_on.txt",
+        "w1/6_divisible_pairs_data_set_example.txt",
     ]
 
     # get the input becasue now I am setup to handle the hidden test cases
-    n, k, ar, expected = parse_input(possible_inputs[3])
+    n, k, ar, expected = parse_input(possible_inputs[0])
 
     # and now get the result
-    result = divisibleSumPairs1(n, k, ar)
-    # result = divisibleSumPairs2(n, k, ar)
+    #result = divisibleSumPairs1(n, k, ar)
+    result = divisibleSumPairs2(n, k, ar)
 
     try:
         assert result == expected
