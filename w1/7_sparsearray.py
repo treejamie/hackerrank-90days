3fc89626c7f652a0f0d138357dc718413d8d7631
Jamie C2 <human@jamiec2.com>
2025-01-26 10:41:11 +0000

    that works nicely

--- w1/7_sparsearray.py
+++ w1/7_sparsearray.py
@@ -1,11 +1,7 @@
 import sys
 
-def matchstrings(strings, queries):
-    
-    pass
+import re
 
-
-    return 0
 
 
 def parse(fname):
@@ -45,35 +41,68 @@ def parse(fname):
     # and done
     return scount, strings, qcount, queries
 
+
+def matchstrings1(strings, queries):
+    """
+    Let us just do an O(n2) first. We win no friends, but we do get a tick.
+    """
+
+    # somewhere to stash the results
+    results = []
+
+    # iterate
+    for q in queries:
+
+        # the regex is dynamic, so precompile it
+        regex = re.compile(r'^{0}$'.format(q))
+
+        # get the matches
+        matches = [re.match(regex, s) for s in strings]
+
+        # filter out the Nones
+        result = len([m for m in matches if m is not None])
+
+        # append to the results
+        results.append(result)
+    
+    # and return the results
+    return results
+
+
+
+
 if __name__ == "__main__":
     
     # the data to work with
-    datasets = [
-        "w1/7_sparsearray_data_1.txt",
-        "w1/7_sparsearray_data_2.txt",
-        "w1/7_sparsearray_data_3.txt",
-    ]
+    datasets = {
+        "w1/7_sparsearray_data_1.txt": [2, 1, 0],
+        "w1/7_sparsearray_data_2.txt": [1, 0, 1],
+        "w1/7_sparsearray_data_3.txt": [1, 3, 4, 3, 2],
+    }
 
     # iterate
-    for x in datasets:
+    for fname, expected_results in datasets.items():
         # parse
-        scount, strings, qcount, queries = parse(x)
+        scount, strings, qcount, queries = parse(fname)
 
         # run
-        result = matchstrings(strings, queries)
+        results = matchstrings1(strings, queries)
 
         # assert
         try:
-            assert result == qcount
-            print("✅: success {0} - {1} expected. {2} received".format(
-                x,
-                qcount,
-                result
+            assert results == expected_results
+            print("✅: success {0} - {1} expected and {2} received".format(
+                fname,
+                expected_results,
+                results
             ))
         except AssertionError:
-            msg = "❌: error in {0} - {1} expected. {2} received".format(
-                x,
-                qcount,
-                result
+            msg = "❌: error in {0}:\n\texpected: {1}\n\treceived: {2} ".format(
+                fname,
+                expected_results,
+                results
             )
             sys.exit(msg)
+
+
+
