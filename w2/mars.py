d39106233daf016c35b826cdb90c989606b36111
Jamie <1866+treejamie@users.noreply.github.com>
2025-01-30 14:55:42 +0000

    W2/8 - Mars Exploration (#21)
    * the mars task has been done
    
    ---------
    
    Co-authored-by: treejamie <localadmin@localadmins-MacBook-Pro.local>

---
+++ w2/mars.py
@@ -0,0 +1,68 @@
+import sys
+
+def parse_input(f):
+    with open(f) as fp:
+        s = fp.readline()
+    return s.strip()
+
+
+def mars_exploration(s):
+    # we expect chunks of three, so split it up
+    # into chunks of three that we can count diffs on
+    chunks = []
+    for i, x in enumerate(s):
+        # if not an iteration of three, skip.
+        if i % 3 != 0:
+            continue
+
+        # make the slices
+        l = i
+        r = i + 3
+        
+        # add to the chunks
+        chunks.append((s[l:r]))
+    
+    # now iterate over chunks and count differences
+    diffs = 0
+    for chunk in chunks:
+        for i, char in enumerate("SOS"):
+            if chunk[i] != char:
+                diffs += 1
+
+    return diffs
+
+
+
+
+if __name__ == "__main__":
+    
+    inputs = {
+        "w2/mars_1.txt": 3,
+        "w2/mars_2.txt": 1,
+        "w2/mars_3.txt": 0,
+        "w2/mars_tc3.txt": 12 
+    }
+
+    for k, v in inputs.items():
+        s, expected = parse_input(k), v
+        x = mars_exploration(s)
+
+        # assert
+        try:
+            assert x == expected
+            print("✅: success {0} - {1} expected and {2} received".format(
+                s,
+                expected,
+                x
+            ))
+        except AssertionError:
+            msg = "❌: error in {0}:\n\texpected: {1}\n\treceived: {2} ".format(
+                s,
+                expected,
+                x
+            )
+            sys.exit(msg)
+
+
+
+
