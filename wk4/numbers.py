46b9c4c12d8e404560b3be17db9f015f2cba3d0d
treejamie <me@treejamie.com>
2025-02-12 08:26:18 +0000

    subarrays is now working as expected

--- wk4/numbers.py
+++ wk4/numbers.py
@@ -1,6 +1,6 @@
 import sys
 
-def pickingNumbers(a):
+def pickingNumbers1(a):
     # sort it first
     # a.sort()
 
@@ -31,7 +31,68 @@ def pickingNumbers(a):
 
     # le done
     return max(subs)
+
+
+def in_bounds(x, arr_in, arr_out):
+    # base case is that there no arr_in left
+    if len(arr_in) == 0:
+        return arr_out
     
+    # now we want everything in the array within abs(1)
+    a = []
+    for y in arr_in:
+        if abs(x - y) <= 1:
+            a.append(y)
+    
+    # append a onto array_out
+    arr_out.append(a)
+    
+    # now pop off the end and call again
+    return in_bounds(arr_in.pop(), arr_in, arr_out)
+
+
+def subarrays(x, a_in, a_out):
+    
+    # define distance
+    distance = x + 1
+
+    # get everything within distance of the target
+    z = list(filter(lambda y: y <= distance, a_in))
+
+    # append it to the a_out
+    a_out.append(z)
+
+    # remove everything from a_in that is in a_out
+    foo = [x for x in a_in if x not in z]
+
+    # the base case
+    if len(foo) == 0:
+        return a_out
+
+    # now call the function again
+    return subarrays(foo[0], foo, a_out)
+    
+
+
+def pickingNumbers(a):
+    """
+    Subarrays of a that are less than 1 int away from each other.
+
+    categorised as a frequency counting problem
+    """
+    #  sort the array
+    a.sort()
+
+    # get the first
+    start = a[0]
+
+    # convert everthing into subarrays 
+    subs = subarrays(start, a, [])
+
+    # call max on a list comprehension of len(x) in subs and that is our answer
+    return max([len(x) for x in subs])
+
+
 
 
 tests = [
