c036b0cb38759949082dc72e6e33097f14caf432
treejamie <me@treejamie.com>
2025-03-26 15:49:05 +0000

    password challenge is done

--- w5/passwd.py
+++ w5/passwd.py
@@ -1,31 +1,44 @@
 """
-You will be given a list of integers ($arr) and a single integer $k.
+A password is strong if it satisfies the following criteria:
 
-Create sublists of length $k from elements of $arr such that unfairness is minimised.
-
-unfairness of a sublist ($x) is calculated as max($x) - min($x)
+* Its length is at least 6.
+* It contains at least one digit.
+* It contains at least one lowercase English character.
+* It contains at least one uppercase English character.
+* It contains at least one special character. The special characters are: !@#$%^&*()-+
 
 """
 import sys
 
 
-def minimumNumber(n: int, password: str) -> int:
+def minimum_number(pwlength: int, password: str) -> int:
     """
-    You don't need to calculate all the permutations.
-
-    Sorting the array list is enough so that the values are optimised
-    to remove the need for calculating max(x) - min(x). This uses python
-    internal timsort which is O(n log n).  The loop is O(n), which makes
-    the sort the most intensive complexity.
-
-    So we have an O(n log(n)) complexity.
-    
+    O(n) - this could be O(1) if I looped over it just once.
     """
-    return 1
+    # from the task
+    min_length = 6
+    numbers = "0123456789"
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special_characters = "!@#$%^&*()-+"
 
+    # split up the password into a list
+    password.split()
 
+    # now check everything - except length
+    total_numbers = 0 if len([x for x in password if x in numbers]) > 0 else 1
+    total_lowercase = 0 if len([x for x in password if x in lower_case]) > 0 else 1
+    total_uppercase = 0 if len([x for x in password if x in upper_case]) > 0 else 1
+    total_special = 0 if len([x for x in password if x in special_characters]) > 0 else 1
 
+    # so now we know how many characters we have to add, do length seperately at the end
+    total = total_numbers + total_lowercase + total_uppercase + total_special
 
+    # add total onto current length and see if we have anymore to add
+    total_length = 0 if pwlength + total >= min_length else min_length - pwlength - total
+
+    # now return that total
+    return total + total_length
 
 
 
@@ -42,11 +55,9 @@ tests = [
             1,
             "test 1"
         ]
-
- 
 ]
 
-target = minimumNumber
+target = minimum_number
 
 skip_tests = [
     # "test one",
@@ -61,7 +72,7 @@ for test in tests:
     expected = test.pop()
     result = target(*test)
 
-    # skip tests 
+    # skip tests
     if name in skip_tests:
         continue
 
@@ -70,5 +81,5 @@ for test in tests:
         assert result == expected
         print(f"✅: ({target.__name__}) SUCCESS - {expected} expected and {result} result")
     except AssertionError:
-        msg = f"🛥️ ({target.__name__})FAILBOAT {name} :\n\texpected: {expected}\n\tresult: {result} "
-        sys.exit(msg)
+        MSG = f"🛥️ ({target.__name__})FAILBOAT {name} :\n\texpected: {expected}\n\tresult: {result}"
+        sys.exit(MSG)
