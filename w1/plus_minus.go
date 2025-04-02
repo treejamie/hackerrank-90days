c6f7d92a60faad02dbd55b15cfc627f15c442302
treejamie <me@treejamie.com>
2025-04-02 17:25:00 +0100

    committing for the day - not quite tehre, but kinda getting there to a workable go solution

--- w1/plus_minus.go
+++ w1/plus_minus.go
@@ -1,17 +1,54 @@
+/*
+This is the first challenge of week 1.
+
+https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem
+
+Given an array of integers, calculate the ratios of its elements that are
+positive, negative, and zero. Print the decimal value of each fraction on a
+new line with 6 places after the decimal.
+*/
 package w1
 
 import "fmt"
 
+func PlusMinus(arr []int32) []float64 {
 
-func PlusMinus(arr []int32) float32 {
+	// the length is used a lot, so calculate that once
+	l := float64(len(arr))
 
-	fmt.Println(arr)
-	fmt.Println("Hello")
-	return 0.1
+	// make three slices for each of the values
+	p, n, z := []int32{}, []int32{}, []int32{}
+
+	// fill up the slice dependant on the value
+	for _, value := range arr {
+		// three cases, I know, I know, I could have used ifs
+		// but just switch on true instead.
+		switch {
+		case value > 0:
+			p = append(p, value)
+		case value < 0:
+			n = append(n, value)
+		default:
+			z = append(z, value)
+		}
+	}
+	// now calculate the ratios
+	var p_ratio float64 = float64(len(p)) / l 
+	var n_ratio float64 = float64(len(n)) / l 
+	var z_ratio float64 = float64(len(z)) / l 
+
+	// solve the solution
+	solution := []float64{
+		p_ratio,
+		n_ratio,
+		z_ratio,
+	}
+
+	// Hackerank wants you to print them
+	for _, value := range solution {
+		fmt.Printf("%.6f", value)
+	}
+
+	// return the solution
+	return solution
 }
-
-func main(){
-	arr := []int32{1, 2, 4}
-	PlusMinus(arr)
-}
-
--- w1/plus_minus_test.go
+++ w1/plus_minus_test.go
@@ -2,11 +2,63 @@ package w1
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+// the most minimal function to read in the test case files.
+// they should all be formatted in the right way, so error
+// handling is not a massive concern
+func ReadLines(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	check(err)
+	return strings.Split(strings.TrimSpace(string(data)), "\n"), nil
+}
+
 func Test_plus_minus(t *testing.T){
-	arr := []int32{1, 2, 4}
-	PlusMinus(arr)
-	fmt.Println("the test")
+	// test cases 
+	tcs := []string{
+		"tc/1_0.txt",
+		"tc/1_1.txt",
+		"tc/1_2.txt",
+	}
+
+	// now iterate over the test cases
+	for _, tc := range tcs {
+		// read in the files
+		lines, _ :=  ReadLines(tc)
+		// don't want the first line, but want the second
+		// and split it up into a slice.
+		parts := strings.Fields(lines[1])
+		args := make([]int32, len(parts))
+		for i, arg := range parts{
+			n, _ := strconv.ParseInt(arg, 10, 32)
+			args[i] = int32(n)
+
+		}
+		expected := []string{
+			strings.TrimSpace(lines[3]),
+			strings.TrimSpace(lines[4]),
+			strings.TrimSpace(lines[5]),
+		}
+
+
+
+		// call the function
+		got := PlusMinus(args)
+
+		// 
+		// fmt.Println(args)
+		fmt.Println(expected)
+		fmt.Println(got)
+
+	}
 }
