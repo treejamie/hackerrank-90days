038a5d871010c42fe3f093156fd5ba12568402ab
treejamie <me@treejamie.com>
2025-04-05 16:20:14 +0100

    The first elixir test, with actions to test it

--- mix.exs
+++ mix.exs
@@ -1,6 +1,10 @@
 defmodule Hackerank.MixProject do
   use Mix.Project
 
+  @doc """
+  The most basic mix project that I need to get these elixir
+  challenges behaving like an actual Elixir project.
+  """
   def project do
     [
       app: :hackerank,
@@ -8,6 +12,7 @@ defmodule Hackerank.MixProject do
       elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
-  defp elixirc_paths(:test), do: ["w1"]
-  defp elixirc_paths(_), do: ["w1"]
+
+  defp elixirc_paths(:test), do: ["tests", "w1", "w2", "w3", "w4", "w5"]
+  defp elixirc_paths(_), do: ["lib"]
 end
