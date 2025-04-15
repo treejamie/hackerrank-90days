af1e186327b3eaacd673234ebcdf3d32ffcfe569
Jamie <1866+treejamie@users.noreply.github.com>
2025-04-15 19:57:41 +0100

    upgrade W1/3 (Python) (#109)
    The first week of challenges are weak and poorly executed. There's much
    left on the table such as doctstrings, annotations and pep8 issues.
    
    This PR adds to #107

---
+++ w1/time_converstion_test.py
@@ -0,0 +1,32 @@
+"""
+Tests for Time Conversion
+"""
+import unittest
+from pathlib import Path
+from test import testutils
+from w1.time_conversion import time_conversion
+
+
+
+class TimeConversionTest(unittest.TestCase):
+    """
+    Time conversion test cases
+    """
+    tcs = [
+        "w1/tc/3_0.txt"
+    ]
+
+    def test_time_conversion(self):
+        """Testing Time Conversion - Challenge 3 - Week 1"""
+        for tc in self.tcs:
+            # read in the lines
+            lines = list(testutils.read_lines(Path(tc)))
+
+            # args: line 1 - but it needs splitting up and turning into a list of ints
+            args = lines[0].strip()
+
+            # expected: line 3 - also needs splitting up into ints
+            expected = lines[2].strip()
+
+            # it returns what we expect
+            self.assertEqual(time_conversion(args), expected)
