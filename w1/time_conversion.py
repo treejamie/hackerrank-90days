fae0c1680580968b4b433db693fedbca44de112a
treejamie <me@treejamie.com>
2025-04-15 19:56:58 +0100

    Challenge three is upgraded

--- w1/time_conversion.py
+++ w1/time_conversion.py
@@ -1,20 +1,31 @@
 """
 Time Conversion - Challenge 3 - Week 1
 """
-
 from datetime import datetime
 
+# default to not printing
+PRINT = False
 
-s = "07:05:45PM"
 
-def time_conversion(s):
-    # Write your code here
+def time_conversion(s:str) -> str:
+    """
+    Takes a 12 hour AM/PM string and returns it military time.
+
+    Parameters:
+        s (str): A 12 hour AM/PM string
+    
+    Returns:
+        str: A military formatted time
+
+    Example:
+        >>> time_conversion('07:05:45PM')
+    """
     # first get it from a string into a datetime
     # note: gotcha when parsing the hour - 12 hour parse - not 24!
-    dt = datetime.strptime(s, "%I:%M:%S%p")
+    dt = datetime.strptime(s, '%I:%M:%S%p')
 
     # now remake it as the expected format
-    result = datetime.strftime(dt, "%H:%M:%S")
-    
+    result = datetime.strftime(dt, '%H:%M:%S')
+
     # return the result
     return result
