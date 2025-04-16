1575f9b7cdee674e25190eb8c74b85202bdc03e2
treejamie <me@treejamie.com>
2025-04-16 08:39:59 +0100

    (upgrade) W1/4 - breaking records (Python)

---
+++ w1/breaking_records_test.py
@@ -0,0 +1,33 @@
+"""
+Tests for Breaking Records
+"""
+import unittest
+from pathlib import Path
+from test import testutils
+from w1.breaking_records import breaking_records
+
+
+
+class BreakRecordsTest(unittest.TestCase):
+    """
+    Breaking records test cases
+    """
+    tcs = [
+        "w1/tc/4_0.txt",
+        "w1/tc/4_1.txt"
+    ]
+
+    def test_breaking_records(self):
+        """Testing Breaking Records - Challenge 4 - Week 1"""
+        for tc in self.tcs:
+            # read in the lines
+            lines = list(testutils.read_lines(Path(tc)))
+
+            # args is lines[1], lines[0] is a count and is not needed
+            args = [int(value) for value in lines[1].split()]
+
+            # expected is lines[3]
+            expected = [int(value) for value in lines[3].split()]
+
+            # we get what we expected
+            self.assertEqual(breaking_records(args), expected)
