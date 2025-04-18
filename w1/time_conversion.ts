3f1a3448609682e102df17eee7859d44264daa5b
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-04-18 08:34:57 +0100

    W1/3 - Time Conversion (TypeScript) (#136)
    I was a in a bit of hurry last night, so I've sharpened up the first
    TypeScript solution.
    
    Correct JSDocs.
    Fixed a bug in the solution where `12:00:00AM` was not returning
    `00:00:00`

--- w1/time_conversion.ts
+++ w1/time_conversion.ts
@@ -1,13 +1,28 @@
+/**
+ * Time Conversion - Challenge 3 - Week 1
+ * https://www.hackerrank.com/challenges/three-month-preparation-kit-time-conversion/problem
+ * 
+ * @param s - A time string in the form 12:40:22AM
+ * @returns - A 'military' formatted time 00:40:22
+ * 
+ * @example
+ * timeConversion("12:40:22AM") // "00:40:22"
+ * timeConversion("07:05:45PM") // "19:05:45"
+ */
 export function timeConversion(s:string): string {
-    console.log(s)
     // get into parts
     const [, hourStr, minStr, secStr, period] =
     s.match(/^(\d{2}):(\d{2}):(\d{2})(AM|PM)$/)!;
 
-    // if last is PM add 12 to hour
+    // hour is the target of our meddling.
     let hour = parseInt(hourStr);
+
+    // if period is PM add 12 to hour.
     if (period === "PM" && hour !== 12) hour += 12;
 
-    // fomat and return
-    return `${hour}:${minStr}:${secStr}`
+    // if period is AM and hour is twelve, then it moves to zero.
+    if (period === "AM" && hour === 12) hour = 0;
+
+    // format and return
+    return `${hour.toString().padStart(2, "0")}:${minStr}:${secStr}`;
 }
