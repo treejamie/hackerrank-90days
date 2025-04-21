d029d94ef7523875b18142d949ba317c056c659e
treejamie <me@treejamie.com>
2025-04-21 09:42:12 +0100

    pausing on the go, I'm in an Elixir mindset

---
+++ w1/time_conversion.go
@@ -0,0 +1,12 @@
+/*
+Time Conversion - Challenge 3 - Week 1
+https://www.hackerrank.com/challenges/three-month-preparation-kit-time-conversion/problem
+*/
+package w1
+
+import "fmt"
+
+func TimeConversion(s string) string{
+	fmt.Println(s)
+	return "asd"
+}
